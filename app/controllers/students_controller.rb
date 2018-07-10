4866f9a2523c729cd261ba7a3839604aee94c02e
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-07-10 14:00:06 -0600

    Adds student index, WIP student creation, WIP student show page

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,29 @@
 class StudentsController < ApplicationController
 
   def index
+    @students = Student.all
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:success] = "You created #{@student.name}"
+      redirect_to student_path(@student)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name)
   end
 end
