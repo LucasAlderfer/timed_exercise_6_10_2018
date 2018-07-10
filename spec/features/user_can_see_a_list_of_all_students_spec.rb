6bcd05981da59515e5fb6aafbe497b6899f51e98
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-07-10 14:45:57 -0600

    Adds ability to redirect to student show page from the students index

--- spec/features/user_can_see_a_list_of_all_students_spec.rb
+++ spec/features/user_can_see_a_list_of_all_students_spec.rb
@@ -13,5 +13,25 @@ describe 'when visiting /students' do
       expect(page).to have_content(student_2.name)
       expect(page).to have_content(student_3.name)
     end
+    it "redirects to a student's show page by clicking the student's name" do
+      student_1 = Student.create!(name:'billy')
+      student_2 = Student.create!(name:'bobby')
+      student_3 = Student.create!(name:'joe')
+
+      visit students_path
+
+      click_on student_1.name
+      expect(current_path).to eq(student_path(student_1))
+
+      visit students_path
+
+      click_on student_2.name
+      expect(current_path).to eq(student_path(student_2))
+
+      visit students_path
+
+      click_on student_3.name
+      expect(current_path).to eq(student_path(student_3))
+    end
   end
 end
